a1efa760db7a2bf9d3ddc7e24c82ba7d72571e46
Benjr70 <benrolf70@gmail.com>
2017-11-20 15:29:21 -0800

    code blocks git hub test

--- PBMM.cpp
+++ PBMM.cpp
@@ -1,6 +1,18 @@
-// in C++
 #include <iostream>
+
+void memoryManager(int memSize, int frameSize);
+
+
 int main()
 {
-std::cout << "Hello, world!\n";
+	std::cout << "Hello, world!\n";
+	return 0;
+}
+
+
+void memoryManager(int memSize, int frameSize){
+
+
+
+
 }
