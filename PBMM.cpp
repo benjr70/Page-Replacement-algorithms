08dff34f2636f576ed4552ef4942891960546aa5
Benjr70 <benrolf70@gmail.com>
2017-11-21 13:17:37 -0800

    user input and line parseing working

--- PBMM.cpp
+++ PBMM.cpp
@@ -12,42 +12,57 @@ int write(int pid, int logical_address);
 int read(int pid, int logical_address);
 void printMemory();
 
+//global varibles
+
+
 
 int main()
 {
 	string line;
 	int arg1 = -99;
 	int arg2 = -99;
-    string temp;
+    char temp[50];
+do{
 	getline(cin,line);
-
     int index = 2;
-
-    while(isdigit(line[index])){
-    temp[index-2] = line[index];
-    index++;
-
+    int tempI = 0;
+    while(isdigit(line[index]) && line[index] != '\0'){
+        temp[tempI] = line[index];
+        index++;
+        tempI++;
     }
-    //cout << temp;
-    //arg1 = atoi(temp);
+    temp[tempI] = '\0';
+    arg1 = atoi(temp);
+    index++;
+    tempI = 0;
+    while(isdigit(line[index]) && line[index] != '\0'){
+        temp[tempI] = line[index];
+        index++;
+        tempI++;
+    }
+    temp[tempI] = '\0';
+    arg2 = atoi(temp);
+
 
     switch(line[0]){
-    case 'M' : memoryManager(arg1, arg2);
-        break;
-    case 'A' : allocate(arg1, arg2);
-        break;
-    case 'W' : write(arg1, arg2);
-        break;
-    case 'R' : read(arg1, arg2);
-        break;
-    case 'D' : deallocate(arg1);
-        break;
-    case 'P' : printMemory();
-        break;
-    default  : cout << "command dose not exist";
-
-
+        case 'M' : memoryManager(arg1, arg2);
+            break;
+        case 'A' : allocate(arg1, arg2);
+            break;
+        case 'W' : write(arg1, arg2);
+            break;
+        case 'R' : read(arg1, arg2);
+            break;
+        case 'D' : deallocate(arg1);
+            break;
+        case 'P' : printMemory();
+            break;
+        case 'E' :
+            break;
+        default  : cout << "command dose not exist \n";
     }
+}while(line != "Exit");
+
 	return 0;
 }
 
@@ -57,11 +72,13 @@ void memoryManager(int memSize, int frameSize){
 cout << "in Memory Manger memSize: " << memSize << "\nFramSize: " << frameSize << "\n";
 
 
+
+
 }
 
 int allocate(int allocSize, int pid){
 
-cout << "In allocate, allocSize: " << allocate << "\npid: " << pid << "\n";
+cout << "In allocate, allocSize: " << allocSize << "\npid: " << pid << "\n";
 
 }
 
@@ -87,7 +104,7 @@ cout << "in read pid: " << pid << "logical_address: " << logical_address << "\n"
 
 void printMemory(){
 
-cout << "in printmemory";
+cout << "in printmemory\n";
 
 
 }
